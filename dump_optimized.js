100211c958a9dad10676e7387af870dd406d5bd1
0x1 <kevin.francart3@gmail.com>
2026-08-17 00:55:11 +0200

    misc

--- dump_optimized.js
+++
@@ -1,3 +0,0 @@
-// Actually I don't have an easy way to run the optimizer on a single file from a script
-// because it's a huge compiler.
-// But I can modify purescript-backend-optimizer to dump the optimizedExpr for "contains"!
--- patch_tco_trace.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i '' 's/let isLoop = maybe false tcoRoleIsLoop refBindings/let isLoop = Debug.trace ("isLoop=" <> show (maybe false tcoRoleIsLoop refBindings) <> " for " <> show refBindings) \\_ -> maybe false tcoRoleIsLoop refBindings/g' src/PureScript/Backend/Optimizer/Codegen/Tco.purs
